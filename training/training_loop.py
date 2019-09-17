df76e084c070ce0566cbb50ba5ba17dceccc3640
Shawn Presser <shawnpresser@gmail.com>
2019-09-16 22:57:17 -0500

    By default, resume training from latest snapshot (#19)
    * By default, resume training from latest snapshot
    
    * Define locate_latest_pkl

--- training/training_loop.py
+++ training/training_loop.py
@@ -135,7 +135,7 @@ def training_loop(
     network_snapshot_ticks  = 10,       # How often to export network snapshots?
     save_tf_graph           = False,    # Include full TensorFlow computation graph in the tfevents file?
     save_weight_histograms  = False,    # Include weight histograms in the tfevents file?
-    resume_run_id           = None,     # Run ID or network pkl to resume training from, None = start from scratch.
+    resume_run_id           = 'latest', # Run ID or network pkl to resume training from, None = start from scratch.
     resume_snapshot         = None,     # Snapshot index to resume training from, None = autodetect.
     resume_kimg             = 0.0,      # Assumed training progress at the beginning. Affects reporting and training schedule.
     resume_time             = 0.0):     # Assumed wallclock time at the beginning. Affects reporting.
